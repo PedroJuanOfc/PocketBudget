d6b501fca7cd79bf819f46b33a77262397653b20
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-07-29 21:43:28 -0300

    chore: setup inicial do projeto PocketBudget com SwiftUI e Core Data

---
+++ PocketBudget/PocketBudgetApp.swift
@@ -0,0 +1,20 @@
+//
+//  PocketBudgetApp.swift
+//  PocketBudget
+//
+//  Created by Pedro Juan Ferreira Saraiva on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PocketBudgetApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
